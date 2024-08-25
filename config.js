f0eb6f5e9c7cf088bd58ef94a1171486ab34eb5e
Awmtea Polythene <omomacck@yahoo.com>
2024-08-25 14:50:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,26 +10,26 @@ module.exports = {
      */
     botname: process.env.BotName || "Queen Anya Bot",
     footer: process.env.Footer || "© Powered By Queen Anya Bot",
-    prefa: process.env.Prefix || "-",
+    prefa: process.env.Prefix || ".",
     themeemoji: process.env.ThemeEmoji || "🎐",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "Awmtea~Polythene",
+    ownernumber: process.env.Owner_Number || "918787651195",
+    instagramId: process.env.Insta || "awmtea._polythene",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "@PikaBotz",
+    author: process.env.Author || "@Awmtea",
     packname: process.env.PackName || "Queen Anya v2 MD",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    socialLink: process.env.Web || "https://github.com/Awmteacck",
+    groupLink: process.env.GcLink || "https://whatsapp.com/channel/0029Vakp5lvI7BeHPAtVmx2a",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority",
     sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
 
     /**
