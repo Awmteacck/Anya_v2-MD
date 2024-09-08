cbc999b424268bc773c89cb1ef33961e1a834fad
Awmtea Polythene <omomacck@yahoo.com>
2024-09-08 10:55:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
     mongoUrl: process.env.MongoDB || "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    sessionId: process.env.SESSION_ID || "a3BmMnh_Queen-Anya_zMzk=",
 
     /**
      * thumbnails and medias
